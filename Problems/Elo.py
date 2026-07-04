66d13dfd3b83af9fea81a1eadbad97020c748aa7
69070029 <69070029@kmitl.ac.th>
2026-07-04 21:45:42 +0700

    Problems

--- Problems/Elo.py
+++ Problems/Elo.py
@@ -5,7 +5,8 @@ AB = input('เป็น A หรือ B: ')
 
 #ให้คำตอบ
 if AB == 'A':
-    print('1/(1+10((RB-RA)/400)')
+    ans = 1/(1+10**((RB-RA)/400))
 elif AB == 'B':
-    print('1/(1+10((RA-RB)/400)')
-    
+    ans = 1/(1+10**((RA-RB)/400))
+
+print(round(ans, 2))
