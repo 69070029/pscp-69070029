3d0fa3600cb45f7e65f6ddd307aa5a650f94baf0
69070029 <69070029@kmitl.ac.th>
2026-07-04 21:40:33 +0700

    Problems

---
+++ Problems/Elo.py
@@ -0,0 +1,11 @@
+#ระบุตัวเลข
+RA = int(input('ระบุ RA: '))
+RB = int(input('ระบุ RB: '))
+AB = input('เป็น A หรือ B: ')
+
+#ให้คำตอบ
+if AB == 'A':
+    print('1/(1+10((RB-RA)/400)')
+elif AB == 'B':
+    print('1/(1+10((RA-RB)/400)')
+    
