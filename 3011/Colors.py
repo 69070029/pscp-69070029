9738a2e2820ac405f4f35f6d4e54509c14835dfa
69070029 <69070029@kmitl.ac.th>
2026-07-12 13:10:10 +0700

    Colors

--- 3011/Colors.py
+++ 3011/Colors.py
@@ -1,12 +1,12 @@
 """COLORS"""
-color1 = str(input())
-color2 = str(input())
+Color1 = str(input())
+Color2 = str(input())
 
-if color1 == "Red" and color2 == "Yellow" or color1 == "Yellow" and color2 == "Red":
+if Color1 == "Red" and Color2 == "Yellow" or Color1 == "Yellow" and Color2 == "Red":
     print("Orange")
-elif color1 == "Red" and color2 == "Blue" or color1 == "Blue" and color2 == "Red":
+elif Color1 == "Red" and Color2 == "Blue" or Color1 == "Blue" and Color2 == "Red":
     print("Violet")
-elif color1 == "Yellow" and color2 == "Blue" or color1 == "Blue" and color2 == "Yellow":
+elif Color1 == "Yellow" and Color2 == "Blue" or Color1 == "Blue" and Color2 == "Yellow":
     print("Green")
 else:
     print("Error")
