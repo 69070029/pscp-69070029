55fde7d67f8540fa1610bc2d2615ef969429e92e
029 superdry <69070029@kmitl.ac.th>
2026-07-21 23:31:31 +0700

    kid mai ogg

--- 3024/Vote.py
+++ 3024/Vote.py
@@ -4,10 +4,18 @@ def main():
     sum = float(input())
     high = float(input())
     remain = sum - high
-    
 
-    if 
+    if high <= 3:
+        ans = "Not surprising"
+    elif remain <= high:
+        ans = "Surprising"
+    else:
+        if:
+            ans = "Not surprising" 
+        elif:
+            ans = "Surprising"
 
     print(ans)
 
 main()
+
