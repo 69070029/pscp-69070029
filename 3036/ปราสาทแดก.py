0f615ac85c04694736b472a04539387424555782
029 superdry <69070029@kmitl.ac.th>
2026-07-21 23:35:41 +0700

    s

---
+++ 3036/ปราสาทแดก.py
@@ -0,0 +1,11 @@
+"""prasart"""
+def main():
+    """input"""
+    start = int(input())
+    str(start)
+
+    if start == "3":
+        ans = 0
+    elif start == "2" or start == "4":
+        ans = 2
+    elif start[]
