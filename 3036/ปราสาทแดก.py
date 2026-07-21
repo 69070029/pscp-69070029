9910ccbd53bb05caf43bc949aaec8c0c4d2ee640
029 superdry <69070029@kmitl.ac.th>
2026-07-21 23:40:35 +0700

    s

--- 3036/ปราสาทแดก.py
+++ 3036/ปราสาทแดก.py
@@ -4,8 +4,7 @@ def main():
     start = int(input())
     str(start)
 
-    if start == "3":
-        ans = 0
-    elif start == "2" or start == "4":
-        ans = 2
-    elif start[]
+    for i in range(1, 100001):
+        print(i)
+
+main()
