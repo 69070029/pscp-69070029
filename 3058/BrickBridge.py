6346bdecfce48a24bcc1662a42c783e858855440
029 superdry <69070029@kmitl.ac.th>
2026-07-26 19:43:55 +0700

    -

--- 3058/BrickBridge.py
+++ 3058/BrickBridge.py
@@ -5,13 +5,14 @@ goal = int(input())
 
 how_much_b = goal // 5 #ใช้กี่ก้อน
 
-remain = goal - (how_much_b * 5) #เหลือความยาวอีกเท่าไหร่
+if how_much_b > b:
+    remain = goal - (b * 5)
+else:
+    remain = goal - (how_much_b * 5) #เหลือความยาวอีกเท่าไหร่
 
 how_much_a = remain // 1 #ใช้ a กี่ก้อน
 
-if how_much_b > b:
-    ANS = -1
-elif how_much_a > a:
+if how_much_a > a:
     ANS = -1
 else:
     ANS = how_much_a
