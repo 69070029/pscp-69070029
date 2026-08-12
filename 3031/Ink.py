673474a499fbc755b2687a393ccab7e79bd8379e
029 superdry <69070029@kmitl.ac.th>
2026-08-12 21:33:37 +0700

    INK

--- 3031/Ink.py
+++ 3031/Ink.py
@@ -1,20 +1,18 @@
 """ink"""
+import math
+
 def main():
     """import n input"""
-    import math
 
     spd_ppl = list(map(int, input().split()))
     pi = 3.1416
 
-    speed = (spd_ppl[0] / pi)**0.5
 
     for _ in range(spd_ppl[1]):
         where = list(map(int, (input()).split()))
 
-        distance = (where[0]**2 + where[1]**2)**0.5 #ถูกแล้ว
+        when = math.ceil((pi * (where[0]**2 + where[1]**2)) / spd_ppl[0])
 
-        when = int(math.ceil(distance / speed))
-
-        print(distance)
+        print(when)
 
 main()
