6e21d2833dce84bd281f238880d44e2cb538d14d
029 superdry <69070029@kmitl.ac.th>
2026-08-25 21:43:46 +0700

    3071

---
+++ Problems_7/innovation.py
@@ -0,0 +1,14 @@
+"""งบ"""
+def main():
+    """input"""
+    school = input()
+
+    first = ord(school[0].upper())
+    last = ord(school[-1].upper())
+
+    print(first)
+    #for i in range(len(school)):
+        
+
+
+main()
