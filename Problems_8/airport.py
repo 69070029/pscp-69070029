7972decfe9a06a8d18fbd0ca22aa549617e1f66a
super shy <69070029@kmitl.ac.th>
2026-08-27 17:43:28 +0700

    -

--- Problems_8/airport.py
+++ Problems_8/airport.py
@@ -3,45 +3,53 @@ import math
 
 def main():
     """tumrai"""
-    park = input().split('.')
-    leave = input().split('.')
+    park = input()
+    leave = input()
+    
+    if not "." in (park) or not "." in (leave):
+        print("ERROR")
+        return
 
-    h1 = int(park[0])
-    m1 = int(park[1])
-    h2 = int(leave[0])
-    m2 = int(leave[1])
+    park = park.split('.')
+    leave = leave.split('.')
+    
+    if len(park) != 2 or len(leave) != 2:
+        print("ERROR")
+        return
+
+    h1, m1 = int(park[0]), int(park[1])
+    h2, m2 = int(leave[0]), int(leave[1])
 
     if not (0 <= h1 <= 23 and 0 <= m1 <= 59 and 0 <= h2 <= 23 and 0 <= m2 <= 59):
-        pay = "ERROR"
+        print("ERROR")
+        return
+
+    end = h2 * 60 + m2
+    start = h1 * 60 + m1
+
+    if end < start:
+        end += 24 * 60
+
+    diff = end - start
+
+    if diff <= 15:
+        pay = "FREE"
     else:
-        end = h2 * 60 + m2
-        start = h1 * 60 + m1
-        
-        if end < start:
-            end += 24 * 60
+        hour = math.ceil(diff / 60)
 
-        diff = end - start
+        price = {
+            1: 25,
+            2: 50,
+            3: 80,
+            4: 110,
+            5: 145,
+            6: 180,
+        }
 
-        if diff <= 15:
-            pay = "FREE"
+        if hour in price:
+            pay = price[hour]
         else:
-            hour = math.ceil(diff / 60)
-
-            price = {
-                1: 25,
-                2: 50,
-                3: 80,
-                4: 110,
-                5: 145,
-                6: 180,
-            }
-
-            if hour in price:
-                pay = price[hour]
-            elif hour > 24:
-                pay = "ERROR"
-            else:
-                pay = 250
+            pay = 250
 
     print(pay)
 
