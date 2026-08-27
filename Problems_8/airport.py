51d4e9ab9e19b92682b51d57e82effdb1f0722d6
super shy <69070029@kmitl.ac.th>
2026-08-27 16:46:04 +0700

    airport

--- Problems_8/airport.py
+++ Problems_8/airport.py
@@ -6,31 +6,39 @@ def main():
     park = input().split('.')
     leave = input().split('.')
 
-    minutes = (int(leave[0]) * 60 + int(leave[1])) - (int(park[0]) * 60 + int(park[1]))
+    h1 = int(park[0])
+    m1 = int(park[1])
+    h2 = int(leave[0])
+    m2 = int(leave[1])
 
-    if minutes < 0:
-        minutes += 24 * 60 
-
-    if minutes <= 15:
-        pay = "FREE"
+    if not (0 <= h1 <= 23 and 0 <= m1 <= 59 and 0 <= h2 <= 23 and 0 <= m2 <= 59):
+        pay = "ERROR"
     else:
-        hour = math.ceil(minutes / 60)
+        minutes = (h2 * 60 + m2) - (h1 * 60 + m1)
 
-        price = {
-            1: 25,
-            2: 50,
-            3: 80,
-            4: 110,
-            5: 145,
-            6: 180,
-        }
+        if minutes < 0:
+            minutes += 24 * 60
 
-        if hour in price:
-            pay = price[hour]
-        elif hour > 24:
-            pay = "ERROR"
+        if minutes <= 15:
+            pay = "FREE"
         else:
-            pay = 250
+            hour = math.ceil(minutes / 60)
+
+            price = {
+                1: 25,
+                2: 50,
+                3: 80,
+                4: 110,
+                5: 145,
+                6: 180,
+            }
+
+            if hour in price:
+                pay = price[hour]
+            elif hour > 24:
+                pay = "ERROR"
+            else:
+                pay = 250
 
     print(pay)
 
