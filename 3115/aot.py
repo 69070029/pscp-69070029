02a24b7ef73ce64188135f81a04f2a98462d6708
029 superdry <69070029@kmitl.ac.th>
2026-08-27 20:24:38 +0700

    -

--- 3115/aot.py
+++ 3115/aot.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """arcade of time"""
 def main():
     """input"""
@@ -12,3 +13,21 @@ def main():
 
 
 main()
+=======
+"""aot"""
+num, _ = map(int, input().split())
+
+s_time = [0] * 1441
+
+for _ in range(num):
+    start, stop = map(int, input().split())
+    s_time[start] += 1
+    s_time[stop] -= 1
+
+for i in range(1, 1441):
+    s_time[i] += s_time[i - 1]
+
+times = map(int, input().split())
+
+print(*[s_time[t] for t in times])
+>>>>>>> 0e3d4440b5f35d3b8a4ee55e92e46594ef91bf7c
