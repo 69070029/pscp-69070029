4382322ed6c42f31c5df830987646fa80511fef7
029 superdry <69070029@kmitl.ac.th>
2026-09-10 01:16:35 +0700

    -

--- midterm/Problems_10/13.py
+++ midterm/Problems_10/13.py
@@ -1,43 +1,39 @@
 """hotel"""
-def main():
-    """input"""
-    num = input()
+num = input()
 
-    if int(num[0]) > 5:
-        first = '9'
-    elif int(num[1]) > 5:
-        first = '10'
-    elif int(num[2]) > 5:
-        first = '11'
-    elif int(num[3]) > 5:
-        first = '12'
-    elif int(num[4]) > 5:
-        first = '14'
+if int(num[0]) > 5:
+    FIRST = '9'
+elif int(num[1]) > 5:
+    FIRST = '10'
+elif int(num[2]) > 5:
+    FIRST = '11'
+elif int(num[3]) > 5:
+    FIRST = '12'
+elif int(num[4]) > 5:
+    FIRST = '14'
+else:
+    FIRST = '13'
+
+if num == num[::-1]:
+    if int(num[0]) + int(num[4]) > 5:
+        SEC = '1'
+    elif int(num[1]) * int(num[3]) > 5:
+        SEC = '2'
     else:
-        first = '13'
-
-    if num == num[::-1]:
-        if int(num[0]) + int(num[4]) > 5:
-            sec = '1'
-        elif int(num[1]) * int(num[3]) > 5:
-            sec = '2'
-        else:
-            sec = '0'
+        SEC = '0'
+else:
+    if int(num[4]) and (int(num[0]) // int(num[4]) > 5):
+        SEC = '1'
+    elif int(num[1]) - int(num[4]) > 5:
+        SEC = '2'
     else:
-        if int(num[4]) and (int(num[0]) // int(num[4]) > 5):
-            sec = '1'
-        elif int(num[1]) - int(num[4]) > 5:
-            sec = '2'
-        else:
-            sec = '0'
+        SEC = '0'
 
-    if int(num[0]) + int(num[1]) + int(num[2]) + int(num[3]) + int(num[4]) > 25:
-        third = '1'
-    elif int(num[0]) * int(num[1]) * int(num[2]) * int(num[3]) * int(num[4]) > 55:
-        third = '2'
-    else:
-        third = '0'
+if int(num[0]) + int(num[1]) + int(num[2]) + int(num[3]) + int(num[4]) > 25:
+    THIRD = '1'
+elif int(num[0]) * int(num[1]) * int(num[2]) * int(num[3]) * int(num[4]) > 55:
+    THIRD = '2'
+else:
+    THIRD = '0'
 
-    print(first + sec + third)
-
-main()
+print(FIRST + SEC + THIRD)
