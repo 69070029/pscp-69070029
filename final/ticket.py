379e2c9afa39458f622c13f4c529ef7f0141c1a8
super shys <69070029@kmitl.ac.th>
2026-09-25 14:21:06 +0700

    ticket

--- final/ticket.py
+++ final/ticket.py
@@ -1,30 +1,23 @@
 """ticket"""
 seat = int(input())
-price = 150
+PRICE = 150
 
-try:
-    while True:
-        age, need = map(int, input().split())
+while seat > 0:
+    age, need = map(int, input().split())
 
-        if not seat:
-            print(-2)
-            break
-        elif seat < need:
-            print(-2)
-            continue
-        elif age < 15:
-            print(-1)
-            continue
+    if age < 15:
+        print(-1)
+        continue
+    if seat < need:
+        print(-2)
+        continue
 
-        if 15 <= age <= 22:
-            print(int(need * price * 0.8), seat - need, sep=" ")
-            seat -= need
-        elif age >= 60:
-            print(int(need * price * 0.5), seat - need, sep=" ")
-            seat -= need
-        else:
-            print(need * price, seat - need, sep=" ")
-            seat -= need
-
-except EOFError:
-    pass
+    if 15 <= age <= 22:
+        print(int(need * PRICE * 0.8), seat - need, sep=" ")
+        seat -= need
+    elif age >= 60:
+        print(int(need * PRICE * 0.5), seat - need, sep=" ")
+        seat -= need
+    else:
+        print(need * PRICE, seat - need, sep=" ")
+        seat -= need
