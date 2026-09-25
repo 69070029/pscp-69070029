5217849ddd1081b862624330b8e59e2c1e7f5b8d
super shys <69070029@kmitl.ac.th>
2026-09-25 14:11:51 +0700

    faifah

--- final/faifah.py
+++ final/faifah.py
@@ -1,19 +1,20 @@
 """faifa"""
 unit = int(input())
+pay = 0
 
-if 1 <= unit <= 10:
-    pay = 5 * unit
-elif 11 <= unit <= 50:
-    pay = 50 + (7 * (unit - 10))
-elif 51 <= unit <= 100:
-    pay = 330 + (10 * (unit - 50))
-elif 101 <= unit <= 200:
-    pay = 830 + (12 * (unit - 100))
-else:
-    pay = 2030 + (15 * (unit - 200))
+for i in range(1, unit + 1):
+    if 1 <= i <= 10:
+        pay += 5
+    elif 11 <= i <= 50:
+        pay += 7
+    elif 51 <= i <= 100:
+        pay += 10
+    elif 101 <= i <= 200:
+        pay += 12
+    else:
+        pay += 15
+ft = unit * 0.5
+VAT = pay * 0.07
+total = (pay + VAT + ft) + 0.001
 
-satang = pay * 100
-satang += ((pay * 0.07) * 100) + ((unit * 0.5) * 100)
-
-
-
+print(f"{total:.1f}")
